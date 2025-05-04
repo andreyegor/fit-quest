5957ddc2887b21086d8da356536fd46eaa856b47
1eshiy <1eshiy@vk.com>
2025-05-05 00:24:06 +0700

    Initial project structure

---
+++ vite-config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import fg from 'fast-glob';
+
+function getHtmlInputs() {
+    const entries = fg.sync('src/pages/**/index.html');
+    const input: Record<string, string> = {};
+
+    entries.forEach((entry) => {
+        const name = entry
+            .replace(/^src\/pages\//, '') // убираем начальный путь
+            .replace(/\/index\.html$/, ''); // убираем index.html
+
+        input[name || 'main'] = resolve(__dirname, entry);
+    });
+
+    return input;
+}
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: getHtmlInputs()
+        }
+    }
+});
