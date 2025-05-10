039b56be17757b7f01e8a1f1c81dd3d7738aa628
1eshiy <1eshiy@vk.com>
2025-05-11 00:54:28 +0700

    fixes in sign-* layout and styles

---
+++ src/pages/sign-in/main.ts
@@ -0,0 +1,5 @@
+import AbsolutePlacer from "../../scripts/AbsolutePlacer.ts";
+import PasswordInputToggler from "../../scripts/PasswordInputToggler.ts";
+
+new AbsolutePlacer(".footer__background", "top", 175)
+new PasswordInputToggler()
