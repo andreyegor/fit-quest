4cf3ac58c333704b843a41f9187c04b858cf068e
Egor <andreevegor10000@gmail.com>
2025-05-26 01:11:55 +0700

    some stuff

--- backend/src/main/scala/ru/fitquest/backend/Main.scala
+++ backend/src/main/scala/ru/fitquest/backend/Main.scala
@@ -4,3 +4,7 @@ import cats.effect.{IO, IOApp}
 
 object Main extends IOApp.Simple:
   val run = Server.run[IO]
+
+//TODO безопасное храненние логинов и ключей
+//TODO логирование в докере
+//TODO деплой всего вместе
